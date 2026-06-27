16a69b453f430148e05d2a106f8587ec3ed83ec1
Drajet Franst <fransye12345@gmail.com>
2026-06-27 16:47:47 +0700

    add domain frontend

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://planly-academic-website-bqe6ukcoy-arfwjns-projects-b04944cd.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
