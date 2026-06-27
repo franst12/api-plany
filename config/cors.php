13ac36c362fe4778c8397fbeae9a116b0ed03387
Drajet Franst <fransye12345@gmail.com>
2026-06-27 17:12:56 +0700

    change domain in cors again

--- config/cors.php
+++ config/cors.php
@@ -1,21 +1,12 @@
 <?php
 
 return [
-
-    'paths' => ['*'], // <-- Ubah jadi bintang biar SEMUA jalur URL aman dari CORS
-
+    'paths' => ['*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'], // <-- Ubah jadi bintang biar SEMUA domain (Lokal & Vercel) diizinkan penuh
-
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
-
 ];
